ed49a5db7d034671162ef949d2538eecc866fe20
Lucas Leite <carvalho1104@hotmail.com>
2021-04-16 10:10:48 -0300

    useContext, createContext
    Contexto (context) disponibiliza uma forma de passar dados entre a árvore de componentes sem precisar passar props manualmente em cada nível.

--- src/App.js
+++ src/App.js
@@ -1,79 +1,60 @@
-import P from 'prop-types';
-import { useEffect, useState, useMemo, useRef } from 'react';
+
+import { useContext, useState, createContext } from 'react';
 import './App.css';
 
-
- const Post = ({post, handleClick}) => {
-   console.log("Filho renderizou");
-
-   return(
-     <div key ={post.id} className ="post">
-       <h1 onClick={() => handleClick(post.title)}>{post.title}</h1>
-       <p>{post.body}</p>
-     </div>
-   );
- };
-
- Post.propTypes ={
-   post: P.shape ({
-     id: P.number,
-     title: P.string,
-     body: P.string,
-
-   }),
-   handleClick: P.func,
-
- };
-function App() {
-    const [posts, setPosts] = useState([]);
-    const [value, setValue] = useState('');
-    const input = useRef(null);
+const globalState = {
+  title: ' o titulo que contexto',
+  body: 'O body do contexto',
+  counter: 0,
+};
 
 
-    //componente did mount
+const GlobalContext = createContext();
 
-    useEffect(() =>  {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-           .then((r) => r.json())
-           .then((r) => setPosts(r));
-    },[]);
+const Div = ({children}) => {
+  return (
+    <>
+      <H1 />
+      <P />
+    </>
+  );
+};
 
-
-    useEffect(() => {
-      input.current.focus();
-      console.log(input.current);
-    },[value]);
-
-
-
-    const handleClick = (value) => {
-        setValue(value);
-    }
+//eslint-disable-next-line
+const H1 = () => {
+  const theContext = useContext(GlobalContext);
+  const {
+    contextState: {title, counter},
+  } = theContext;
 
   return (
-    <div className="App">
-
-      <p>
-        <input
-        ref={input}
-        type="search"
-        value={value}
-        onChange={(e)=> setValue(e.target.value)}/>
-      </p>
-
-      {useMemo(() => {
-        return(
-          posts.length > 0 &&
-          posts.map((post) => {
-            return <Post key = {post.id} post={post}  handleClick={handleClick} />;
-          })
-        );
+    <h1>
+      {title}{counter}
+    </h1>
+  );
+};
 
 
+const P = () => {
+  const theContext = useContext(GlobalContext);
+  const {
+    contextState: {body, counter},
+    setContextState,
+  } =theContext;
 
-      }, [posts])}
-      {posts.length <= 0 && <p>Ainda não existem posts</p>}
-    </div>
+  return(
+    <p onClick={() => setContextState((s)=>({...s,counter: s.counter +1 }))}>
+      {body}{counter}
+    </p>
+  );
+}
+
+function App() {
+  const [contextState, setContextState] = useState(globalState);
+  return (
+    <GlobalContext.Provider value={{contextState, setContextState}}>
+      <Div />
+    </GlobalContext.Provider>
   );
 }
 
